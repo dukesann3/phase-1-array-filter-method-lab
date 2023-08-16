2d3f296a4ce5c0c1a1a2aa8bda6462d413af0815
Duke <dukemorise33@gmail.com>
2023-08-15 20:02:41 -0700

    completed phase-1 filter lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+
+function findMatching(arr,matchFilter){
+    //made both into uppercase to keep it the same case...
+    return arr.filter((item) => item.toUpperCase() === matchFilter.toUpperCase());
+}
+
+function fuzzyMatch(arr,matchFilter){
+    return arr.filter((item) => item.indexOf(matchFilter)===0 ? true : false);
+}
+
+function matchName(arr,matchFilter){
+    return arr.filter((item) => item.name === matchFilter ? true : false);
+}
+
+
+
+
+
+
